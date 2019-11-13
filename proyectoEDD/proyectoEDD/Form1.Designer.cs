cca0ba94b4485520f55cc3db7102833728987acf
Jennifer Ramirez <54926972+jenyramirez@users.noreply.github.com>
2019-11-13 10:23:33 -0600

    Proyecto de clase, Estructuras de Datos

---
+++ proyectoEDD/proyectoEDD/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace proyectoEDD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.contrasena = new System.Windows.Forms.TextBox();
+            this.LOGIN = new System.Windows.Forms.Button();
+            this.registro = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::proyectoEDD.Properties.Resources.avatar;
+            this.pictureBox1.Location = new System.Drawing.Point(125, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 106);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::proyectoEDD.Properties.Resources.avatarUSer;
+            this.pictureBox2.Location = new System.Drawing.Point(50, 166);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 36);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::proyectoEDD.Properties.Resources.candado2;
+            this.pictureBox3.Location = new System.Drawing.Point(52, 208);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // usuario
+            // 
+            this.usuario.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuario.Location = new System.Drawing.Point(96, 173);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(182, 29);
+            this.usuario.TabIndex = 3;
+            // 
+            // contrasena
+            // 
+            this.contrasena.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contrasena.Location = new System.Drawing.Point(96, 217);
+            this.contrasena.Name = "contrasena";
+            this.contrasena.Size = new System.Drawing.Size(182, 29);
+            this.contrasena.TabIndex = 4;
+            // 
+            // LOGIN
+            // 
+            this.LOGIN.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.LOGIN.Font = new System.Drawing.Font("Poor Richard", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LOGIN.Location = new System.Drawing.Point(52, 268);
+            this.LOGIN.Name = "LOGIN";
+            this.LOGIN.Size = new System.Drawing.Size(226, 38);
+            this.LOGIN.TabIndex = 5;
+            this.LOGIN.Text = "LOGIN";
+            this.LOGIN.UseVisualStyleBackColor = false;
+            // 
+            // registro
+            // 
+            this.registro.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.registro.Font = new System.Drawing.Font("Poor Richard", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registro.Location = new System.Drawing.Point(52, 312);
+            this.registro.Name = "registro";
+            this.registro.Size = new System.Drawing.Size(226, 38);
+            this.registro.TabIndex = 6;
+            this.registro.Text = "Registrate";
+            this.registro.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::proyectoEDD.Properties.Resources.fondoLOGIN3;
+            this.ClientSize = new System.Drawing.Size(357, 386);
+            this.Controls.Add(this.registro);
+            this.Controls.Add(this.LOGIN);
+            this.Controls.Add(this.contrasena);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.TextBox contrasena;
+        private System.Windows.Forms.Button LOGIN;
+        private System.Windows.Forms.Button registro;
+    }
+}
+
